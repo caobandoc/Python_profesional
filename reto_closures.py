522ec67443503b42842069d7228534084072c5ab
caobandoc <obandocarlosandres@gmail.com>
2022-01-19 21:05:05 -0500

    closures

--- reto_closures.py
+++ reto_closures.py
@@ -8,10 +8,10 @@ def run():
     print(division_by_3(18)) # Output is 6
 
     division_by_5 = make_division_by(5)
-    print(division_by_3(100)) # Output is 20
+    print(division_by_5(100)) # Output is 20
 
-    division_by_5 = make_division_by(18)
-    print(division_by_3(54)) #Output is 3
+    division_by_18 = make_division_by(18)
+    print(division_by_18(54)) #Output is 3
     
 if __name__ == '__main__':
     run()
