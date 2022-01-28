8f5719f63a9f82005bfdc85454a00f1075cf3636
caobandoc <obandocarlosandres@gmail.com>
2022-01-27 21:12:39 -0500

    Generadores

---
+++ generatos.py
@@ -0,0 +1,27 @@
+import time
+
+def fibo_gen(max:int):
+    n1 = 0
+    n2 = 1
+    counter = 0
+    while True:
+        counter += 1
+        if counter == 1:
+            yield n1
+        elif counter == 2:
+            yield n2
+        else:
+            aux = n1 + n2
+            if aux > max:
+                raise StopIteration
+            n1, n2 = n2, aux
+            yield aux
+
+if __name__ == "__main__":
+    fibonnaci = fibo_gen(1000)
+    try:
+        for element in fibonnaci:
+            print(element)
+            time.sleep(0.5)
+    except:
+        print("Termino de iterar hasta 1000")
