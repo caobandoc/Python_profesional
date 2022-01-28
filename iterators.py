052fe6c68f8e9cf5fcf51e54d616946529388845
caobandoc <obandocarlosandres@gmail.com>
2022-01-27 20:24:49 -0500

    iteradores

---
+++ iterators.py
@@ -0,0 +1,29 @@
+import time
+
+class FiboIter():
+    
+    def __iter__(self):
+        self.n1 = 0
+        self.n2 = 1
+        self.counter = 0
+        return self
+    
+    def __next__(self):
+        self.counter +=1
+        if self.counter == 1:
+            return self.n1
+        elif self.counter == 2:
+            return self.n2
+        else:
+            self.aux = self.n1+ self.n2
+            #self.n1 = self.n2
+            #self.n2 = self.aux
+            #swap
+            self.n1, self.n2 = self.n2, self.aux
+            return self.aux
+        
+if __name__ == "__main__":
+    fibonacci = FiboIter()
+    for element in fibonacci:
+        print(element)
+        time.sleep(1)
